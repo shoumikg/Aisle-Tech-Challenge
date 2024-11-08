904ddcb1eecc40c8acf839902fa5a0417398f162
Shoumik Ghosal <shoumikg@iitbhilai.ac.in>
2024-11-08 19:44:54 +0530

    Providing correct configuration and styling to UI elements on PhoneNumberScreen

--- AisleTechChallenge/Views/PhoneNumberViewController.swift
+++ AisleTechChallenge/Views/PhoneNumberViewController.swift
@@ -11,13 +11,14 @@ class PhoneNumberViewController: UIViewController {
     
     @IBOutlet weak var heading: UILabel! {
         didSet {
-            heading.font = UIFont(name: "InterVariable-Medium", size: 30)
+            heading.font = UIFont(name: "InterVariable-ExtraBold", size: 30)
+            heading.text = "Enter Your \nPhone Number"
         }
     }
     
     @IBOutlet weak var subHeading: UILabel! {
         didSet {
-            heading.font = UIFont(name: "InterVariable-Medium", size: 18)
+            subHeading.font = UIFont(name: "InterVariable-Medium", size: 18)
         }
     }
     
@@ -25,25 +26,35 @@ class PhoneNumberViewController: UIViewController {
     
     @IBOutlet weak var countryCodeField: UITextField! {
         didSet {
-            countryCodeField.font = UIFont(name: "InterVariable-Medium", size: 18)
+            countryCodeField.font = UIFont(name: "InterVariable-Bold", size: 18)
+            countryCodeField.textAlignment = .center
+            countryCodeField.keyboardType = .phonePad
         }
     }
     
     @IBOutlet weak var phoneNumberField: UITextField! {
         didSet {
-            countryCodeField.font = UIFont(name: "InterVariable-Medium", size: 18)
+            phoneNumberField.font = UIFont(name: "InterVariable-Bold", size: 18)
+            phoneNumberField.textAlignment = .center
+            phoneNumberField.keyboardType = .phonePad
+        }
+    }
+    
+    @IBOutlet weak var otpField: UITextField! {
+        didSet {
+            otpField.font = UIFont(name: "InterVariable-Bold", size: 18)
         }
     }
     
     @IBOutlet weak var continueButton: UIButton! {
         didSet {
-            continueButton.titleLabel?.font = UIFont(name: "InterVariable-Medium", size: 14)
+            continueButton.titleLabel?.font = UIFont(name: "InterVariable-Bold", size: 14)
         }
     }
     
     @IBOutlet weak var countdownText: UILabel! {
         didSet {
-            countdownText.font = UIFont(name: "InterVariable-Medium", size: 14)
+            countdownText.font = UIFont(name: "InterVariable-Bold", size: 14)
         }
     }
     
@@ -54,5 +65,9 @@ class PhoneNumberViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+    }
+    
+    override func viewDidAppear(_ animated: Bool) {
+        countryCodeField.becomeFirstResponder()
     }
 }
