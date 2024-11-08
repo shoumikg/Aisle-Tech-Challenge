835cb47b0e28839d65e8df1a0616ae532a736659
Shoumik Ghosal <shoumikg@iitbhilai.ac.in>
2024-11-08 19:57:17 +0530

    Adding Model structs for API Requst encoding

---
+++ AisleTechChallenge/Models/AisleModels.swift
@@ -0,0 +1,17 @@
+//
+//  AisleModels.swift
+//  AisleTechChallenge
+//
+//  Created by Shoumik on 08/11/24.
+//
+
+import Foundation
+
+struct PhoneNumberModel {
+    let number: String
+}
+
+struct OTPModel {
+    let number: String
+    let otp: String
+}
